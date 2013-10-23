3a15b4940f02e6aa4457ddff347e45d50c2cac0b
orsomannaro <lewis501>
2013-10-23 18:24:29 +0200

    Messaggi server: versione decente

--- apps/agaimp/__init__.py
+++ apps/agaimp/__init__.py
@@ -1,14 +1,9 @@
-import logging
 import wx
 
 from apps.localparam.controllers import localparam
 from apps.server.publisher import SRV_NAME, SRV_MSG_LVL, SRV_MSG_HMS, SRV_MSG_TXT, ERR_SRV_MSG_LVL
 
-from .controllers import aGaiMpSysApp
-from .views import wxLogHandler, aGaiMpMessages
-
-
-logger = logging.getLogger(__name__)
+from .controllers import aGaiMpSysApp, aGaiMpMessages
 
 
 class aGaiMp(wx.App):
@@ -21,40 +16,32 @@ class aGaiMp(wx.App):
 
         # Systray app
         menu = [
-            ('Exit', self.OnClose),
-            ('Messaggi', self.OnShowMessages),
-            ('Parametri', self.OnEditParams),
+            ('Exit', self.on_close),
+            ('Messaggi', self.on_show_messages),
+            ('Parametri', self.on_edit_params),
         ]
         self._systrayapp = aGaiMpSysApp(None, menu)
 
-        # Messaggi dai server
-        self._gui = aGaiMpMessages(None)
+        # Gestore messaggi server
+        self._messages = aGaiMpMessages(None)
 
-        FORMAT = "%(asctime)s %(levelname)s %(message)s"
-        handler = wxLogHandler(self._gui.txt_messages)
-        handler.setFormatter(logging.Formatter(FORMAT))
-        logger.addHandler(handler)
-        logger.setLevel(logging.DEBUG)
+    def on_close(self, event):
+        # TODO: bisognerebbe gestire arresto dei server
+        self._messages.close()
+        self._systrayapp.close()
+        self.Exit()
 
-    def OnClose(self, event):
-        #TODO: bisognerebbe gestire arresto dei server
-        self.exit()
-
-    def OnEditParams(self, event):
+    def on_edit_params(self, event):
         localparam.edit()
 
-    def OnShowMessages(self, event):
+    def on_show_messages(self, event):
         self._systrayapp.set_default_icon()
-        self._gui.Show()
+        self._messages.show()
 
     def publish(self, message):
         """ Messaggi in arrivo dai server
         """
         if message[SRV_MSG_LVL] == ERR_SRV_MSG_LVL:  # messaggio di errore
-            msg = '[%s %s] %s\n' % (message[SRV_NAME], message[SRV_MSG_HMS], message[SRV_MSG_TXT])
+            msg = '[%s %s] %s' % (message[SRV_NAME], message[SRV_MSG_HMS], message[SRV_MSG_TXT])
             #self._systrayapp.set_error_icon('aGaiMp: fai click su Messaggi')
-            logger.log(logging.DEBUG, "More? click again!")
-
-    def exit(self):
-        self._systrayapp.close()
-        self.Exit()
+            self._messages.log(msg)
--- apps/agaimp/controllers.py
+++ apps/agaimp/controllers.py
@@ -1,3 +1,9 @@
+import wx
+
+from wx.lib.newevent import NewEvent
+
+from gui.agaimp_gui import frm_srv_msg
+
 from settings import TRAY_ICON, TRAY_TOOLTIP, TRAY_ICON_WRN, TRAY_ICON_ERR
 
 from .views import SystrayApp
@@ -24,3 +30,40 @@ class aGaiMpSysApp(SystrayApp):
         :param tooltip: eventuale tooltip di errore
         """
         self.set_icon(TRAY_ICON_WRN, tooltip)
+
+
+wxShowMessageEvent, EVT_WX_SHOW_MSG = wx.lib.newevent.NewEvent()
+
+class aGaiMpMessages(object):
+    """ Gestisce la visualizzazione dei massaggi in arrivo dal server.
+    """
+
+    def __init__(self, parent):
+        # TODO: mettere in camel case il nome della classe frm_srv_msg
+        self._frm = frm_srv_msg(parent)
+        self._frm.btn_reset.Bind(wx.EVT_BUTTON, self.on_reset)
+        self._frm.btn_close.Bind(wx.EVT_BUTTON, self.on_close)
+        self._frm.txt_messages.Bind(EVT_WX_SHOW_MSG, self.on_log_event)
+
+    def on_close(self, event):
+        self._frm.Hide()
+
+    def on_reset(self, event):
+        pass
+
+    def on_log_event(self, event):
+        if self._frm.IsShown():
+            msg = event.message.strip('\r')+'\n'
+            self._frm.txt_messages.AppendText(msg)
+            #wx.CallAfter(self.txt_messages.AppendText, msg)
+            event.Skip()
+
+    def close(self):
+        self._frm.Close()
+
+    def log(self, message):
+        evt = wxShowMessageEvent(message=message)
+        wx.PostEvent(self._frm.txt_messages, evt)
+
+    def show(self):
+        self._frm.Show()
